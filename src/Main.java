337bb132feff4f991bba8351986255a18707fcf5
ShabievBekjan <beka.shabiev@gmail.com>
2022-07-28 15:49:10 +0600

    Finish code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss();
         boss.setHealthBoss(500);
+        boss.setDamage(20);
+        boss.setDefencetype("Fire");
+        System.out.println("healthBoss = " + boss.getHealthBoss());
+        System.out.println("Damage =" + boss.getDamage());
+        System.out.println("Defencetype" + boss.getDefencetype());
 
     }
 }
